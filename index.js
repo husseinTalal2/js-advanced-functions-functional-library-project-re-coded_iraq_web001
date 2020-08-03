1a403f9059407c950dca3e7372ffddb7c2dfebc4
Hussein Talal <hossientalal2@gmail.com>
2020-08-03 15:35:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: (collection, cb) => {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
+      for (let i = 0; i < newCollection.length; i++)
+        cb(newCollection[i]);
+
+      return collection;
     },
 
     map: function() {
